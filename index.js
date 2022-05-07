2c1cc0f3a890a9fb0d5cd157084ee382ee4f92c0
Aaron Varga <apvarga@student.fullsail.edu>
2022-05-06 22:34:20 -0400

    Updates to routing, still having trouble with first request in activity.

--- index.js
+++ index.js
@@ -12,32 +12,44 @@ app.post("/", (request, response) => {
 });
 
 // GET /products/all HTTP/1.1
-app.get("/products/all", (request, response) => {
-  console.log(request.query.pageNum);
-  console.log(request.query.sortBy);
-  console.log(request.query.orderBy);
-  response.send("Products - Page Number: " + request.query.pageNum);
-});
+// app.get("/products/all", (request, response) => {
+//   console.log(request.query.pageNum);
+//   console.log(request.query.sortBy);
+//   console.log(request.query.orderBy);
+//   response.send(
+//     "Products - Page number: " +
+//       request.query.pageNum +
+//       ", Sort by: " +
+//       request.query.sortBy +
+//       ", Order by: " +
+//       request.query.orderBy
+//   );
+// });
 
 // GET /products/all?sort=price HTTP/1.1
-// app.get("/products/all", (request, response) => {
-//   let sortBy = request.query.sortBy;
-//   response.send("Products - Sort by: " + sortBy);
-// });
+app.get("/products/all", (request, response) => {
+  let sort = request.query.sort;
+  response.send("Products - Sort by: " + sort);
+});
+
+// GET /products/683-nike-large-white-shoe HTTP/1.1
+app.get(
+  "/products/:productId-:productSize-:productColor",
+  (request, response) => {
+    response.send(
+      "Product - Product ID: " +
+        request.params.productId +
+        ", Product Size: " +
+        request.params.productSize +
+        ", Product Color: " +
+        request.params.productColor
+    );
+  }
+);
 
 // GET /products/424 HTTP/1.1
 app.get("/products/:productId", (request, response) => {
   response.send("Product - Product ID: " + request.params.productId);
-});
-
-// GET /products/683-nike-large-white-shoe HTTP/1.1
-app.get("/products/:productId-:productName", (request, response) => {
-  response.send(
-    "Product Page! Product name: " +
-      request.params.productName +
-      ", Product id: " +
-      request.params.productId
-  );
 });
 
 // GET /products/nike-large-white-shoe HTTP/1.1
