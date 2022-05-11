7d62711686b7f612086dbe222a594cf801c17ba3
Aaron Varga <apvarga@student.fullsail.edu>
2022-05-11 09:09:34 -0400

    initial pug setup

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 const productRouter = require("./routes/Products");
+app.set("views", __dirname + "/templates/views");
+app.set("view engine", "pug");
+
+app.get("/", (req, res) => {
+  res.render("home", { title: "Hey", message: "Hello there!" });
+});
 
 app.use("/products", productRouter);
 
