08046ac3a778807ef4652971fa5c99edf629e0e6
Aaron Varga <apvarga@student.fullsail.edu>
2022-05-16 18:43:16 -0400

    solved the create new product function issue.

--- models/Products.js
+++ models/Products.js
@@ -1,34 +1,34 @@
 let products = [
-  { id: "1", slug: "nike-shoe", title: "Nike Shoe" },
-  { id: "2", slug: "adidas-shoe", title: "Adidas Shoe" },
-  { id: "3", slug: "jordans-shoe", title: "Jordans Shoe" },
-  { id: "4", slug: "reebok-shoe", title: "Reebok Shoe" }
+    { id: "1", slug: "nike-shoe", title: "Nike Shoe" },
+    { id: "2", slug: "adidas-shoe", title: "Adidas Shoe" },
+    { id: "3", slug: "jordans-shoe", title: "Jordans Shoe" },
+    { id: "4", slug: "reebok-shoe", title: "Reebok Shoe" }
 ];
 
 const all = () => {
-  return products;
+    return products;
 };
 
 const find = (id) => {
-  return products.find((p) => p.id === id);
+    return products.find((p) => p.id === id);
 };
 
 const create = (product) => {
-  products.id = Number(products[products.length - 1].id) + 1 + "";
-  products.push(product);
-  return products[products.length - 1];
+    product.id = Number(products[products.length - 1].id) + 1 + "";
+    products.push(product);
+    return products[products.length - 1];
 };
 
 const update = (id, product) => {
-  products = products.map((p) => {
-    return p.id === id ? product : p;
-  });
-  return products;
+    products = products.map((p) => {
+        return p.id === id ? product : p;
+    });
+    return products;
 };
 
 const remove = (id) => {
-  products = products.filter((p) => p.id !== id);
-  return products;
+    products = products.filter((p) => p.id !== id);
+    return products;
 };
 
 module.exports = { all, find, create, update, remove };
