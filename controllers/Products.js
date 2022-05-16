c8ef9ae4750d82f4a25167a58c90f23a944158bd
Aaron Varga <apvarga@student.fullsail.edu>
2022-05-16 17:58:45 -0400

    going back through code to figure out why my create function is not working properly.

--- controllers/Products.js
+++ controllers/Products.js
@@ -1,43 +1,43 @@
 const Products = require("../models/Products");
 
 const index = (req, res) => {
-  const products = Products.all();
-  res.render('views/products/index', { products });
-  // res.json(products);
+    const products = Products.all();
+    res.render('views/products/index', { products });
+    // res.json(products);
 };
 
 const form = (req, res) => {
-  // res.send("Product.form");
-  if (typeof req.params.id !== "undefined") {
-    const product = Products.find(req.params.id);
-    res.render('views/products/edit', { product })
-  } else {
-    res.render('views/products/create');
-  }
+    // res.send("Product.form");
+    if (typeof req.params.id !== "undefined") {
+        const product = Products.find(req.params.id);
+        res.render('views/products/edit', { product })
+    } else {
+        res.render('views/products/create');
+    }
 };
 
 const show = (req, res) => {
-  const product = Products.find(req.params.id);
-  res.render('views/products/show', { product })
-  // res.json(product);
+    const product = Products.find(req.params.id);
+    res.render('views/products/show', { product })
+    // res.json(product);
 };
 
 const create = (req, res) => {
-  const product = Products.create(req.body);
-  res.redirect('/products' + product.id);
-  // res.json(product);
+    const product = Products.create(req.body);
+    res.redirect('/products/' + product.id);
+    // res.json(product);
 };
 
 const update = (req, res) => {
-  const product = Products.update(req.params.id, req.body);
-  res.redirect('/products' + req.params.id);
-  // res.json(product);
+    const product = Products.update(req.params.id, req.body);
+    res.redirect('/products/' + req.params.id);
+    // res.json(product);
 };
 
 const remove = (req, res) => {
-  const product = Products.remove(req.params.id);
-  res.redirect('/products');
-  // res.json(product);
+    const product = Products.remove(req.params.id);
+    res.redirect('/products');
+    // res.json(product);
 };
 
 module.exports = { index, form, show, create, update, remove };
--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.set("view engine", "twig");
 app.set("views", __dirname + "/templates");
 
 app.get("/", (req, res) => {
-  res.render("views/home");
+    res.render("views/home");
 });
 
 app.use("/products", productRouter);
