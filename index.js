24b81e2abf289bf200d7532e233bd972ebf8bc36
Aaron Varga <apvarga@student.fullsail.edu>
2022-05-16 10:50:17 -0400

    Reverted back to installing Twig instead of Pug due to too many complications.

--- index.js
+++ index.js
@@ -4,14 +4,10 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 const productRouter = require("./routes/Products");
 app.set("views", __dirname + "/templates/views");
-app.set("view engine", "pug");
+app.set("view engine", "twig");
 
 app.get("/", (req, res) => {
-  res.render("home", { title: "Home", message: "Hello there!", users: [
-      { name: "Aaron Varga", email: "apvarga89@icloud.com" },
-      { name: "Michael Varga", email: "mvargs@gmail.com" },
-      { name: "Sean Varga", email: "seaniebonners@yahoo.com" },
-    ] });
+  res.render("home");
 });
 
 app.use("/products", productRouter);
