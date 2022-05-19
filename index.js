6fee5ff51ce781c0418082c21ce3186be9326410
Aaron Varga <apvarga@student.fullsail.edu>
2022-05-19 13:55:12 -0400

    setup views for images and variants. Trying to figure out why my Image show is not populated the variant in the dropdown when creating a new image. Still troubleshooting.

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 const productRouter = require("./routes/Products");
+const variantRouter = require("./routes/Variants");
+const imageRouter = require("./routes/Images");
 app.set("view engine", "twig");
 app.set("views", __dirname + "/templates");
 
@@ -11,5 +13,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/products", productRouter);
+app.use("/variants", variantRouter);
+app.use("/images", imageRouter);
 
 app.listen(3000);
