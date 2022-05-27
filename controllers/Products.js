634ab3750712414b0d2d6f48d020acb1793eaac7
Aaron Varga <apvarga@student.fullsail.edu>
2022-05-27 12:52:31 -0400

    I was finally able to get the Variants page to show for images. As in, when I go to images, I can now see a dropdown for variants. This was my initial problem early in the week I have been trying to solve.

--- controllers/Products.js
+++ controllers/Products.js
@@ -1,48 +1,40 @@
-const { Product } = require("../models");
+const { Product } = require('../models')
 
 const index = async (req, res) => {
-    const product = await Product.findAll();
-    res.render('views/products/index', { product });
-    // res.json(products);
-};
+    const products = await Product.findAll()
+    res.render('views/products/index', { products })
+    // res.json(products)
+}
 
 const form = async (req, res) => {
-    // res.send("Product.form");
-    if (typeof req.params.id !== "undefined") {
-        const product = await Product.findByPk(req.params.id);
+    if (req.params.id) {
+        const product = await Product.findByPk(req.params.id)
         res.render('views/products/edit', { product })
     } else {
-        res.render('views/products/create');
+        res.render('views/products/create')
     }
-};
+}
 
 const show = async (req, res) => {
-    const product = await Product.findByPk(req.params.id);
-    res.render('views/products/show', { product });
-    // res.json(product);
-};
+    const product = await Product.findByPk(req.params.id)
+    res.render('views/products/show', { product })
+}
 
 const create = async (req, res) => {
-    // const product = Products.create(req.body);
-    const product = await Product.create(req.body);
-    res.redirect('/products/' + product.id);
-    // res.json(product);
-};
+    const product = await Product.create(req.body)
+    res.redirect('/products/' + product.id)
+}
 
-const update = (req, res) => {
-    const product = Product.update(req.body, {
-        where: {
-            id: req.params.id
-        }
-    });
-    res.redirect('/products/' + req.params.id);
-    // res.json(product);
-};
+const update = async (req, res) => {
+    const product = await Product.update(req.body, {
+        where: { id: req.params.id }
+    })
+    res.redirect('/products/' + req.params.id)
+}
 
 const remove = async (req, res) => {
-    const product = await Product.destroy({ where: { id: req.params.id }});
-    res.redirect('/products');
-    // res.json(product);
-};
+    const products = await Product.destroy({ where: { id: req.params.id }})
+    res.redirect('/products')
+}
 
-module.exports = { index, form, show, create, update, remove };
+module.exports = { index, form, show, create, update, remove }
